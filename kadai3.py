ce148ccbc5805a9110b4529e20afd44f539546e1
Fenking <fenking@outlook.com>
2021-07-18 17:11:26 +0800

    简单修改格式

--- kadai3.py
+++ kadai3.py
@@ -4,7 +4,7 @@ import unicodedata
 import MeCab
 index={}
 
-files = glob.glob("wiki/*.txt")
+files = glob.glob("seireishi/*.txt")
 
 if not files:
     print("ファイルのリストが見つかりません")
@@ -21,26 +21,23 @@ for j,file in enumerate(files):
 				line_first=i.split()
 				if line_first[0]=="EOS":
 					pass
-				else:
-                                    line_cut=line_first[1].split(',')
-                                    if line_cut[0]=="名詞":
-                                        if line_first[0] not in index:
-                                            index[line_first[0]]={}
-                                            index[line_first[0]][j]=1
-                                        else:
-                                            if j not in index[line_first[0]]:
-                                                index[line_first[0]][j]=1
-                                            else:
-                                                index[line_first[0]][j]+=1
+				else:#名詞を確認
+					line_cut=line_first[1].split(',')
+					if line_cut[0]=="名詞":
+						if line_first[0] not in index:
+							index[line_first[0]]={}
+							index[line_first[0]][j]=1
+						else:
+							if j not in index[line_first[0]]:
+								index[line_first[0]][j]=1
+							else:
+								index[line_first[0]][j]+=1
 
-#こちらのパソコンは外国語対応するため、txtファイル名前は日本語文字化けと示している。例岩坂 名奈=>娾嶁柤撧
-#そのためこちら転置索引の順番、認識された単語について違いがあるかもしれません。
-#結果の手順はTA／教授での出力結果を基本としてお願いします。
-#また、TAさんチェックする際に、正しい順番のinv.txtファイルをメールで発送していいでしょうか、次の課題に使用するため
 
-with open("inv.txt", "w", encoding="utf-8") as f2:
+
+with open("inv_seireishi.txt", "w", encoding="utf-8") as f2:
 	for i in index:
-		f2.write(format(i,"<25"))
+		f2.write(format(i,"<20"))
 		f2.write(",".join(str(j)+":"+str(index[i][j]) for j in index[i]))
 		f2.write("\n")
 
